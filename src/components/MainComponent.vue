4bf6b8b237635a1308a66fe10934ff0472fe84a9
zer0b2 <xrykin_00@mail.ru>
2022-12-29 23:20:07 +0300

    add handler for list

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="main">
     <ul id="item-list">
-      <li v-for="item in items" :key="item.value">
+      <li
+        v-for="(item, index) in items"
+        :key="item.value"
+        :index="index"
+        v-on:click="change_dec(index)"
+        v-bind:class="[{ crossedout: item.isActive == true }]"
+      >
         {{ item.value }}
       </li>
     </ul>
@@ -13,9 +19,23 @@ export default {
   name: "mainComponent",
   data() {
     return {
-      text: "Firts Project",
-      items: [{ value: "Text 1" }, { value: "Text 2" }],
+      items: [
+        { value: "Text 1", isActive: false },
+        { value: "Text 2", isActive: false },
+      ],
     };
+  },
+  methods: {
+    change_dec: function (index) {
+      console.log(this.items[index].isActive);
+      this.items[index].isActive = !this.items[index].isActive;
+    },
   },
 };
 </script>
+
+<style>
+.crossedout {
+  text-decoration: line-through;
+}
+</style>
