773b4eb70a45dbb3c8bdf855fce4aae82c85c3b2
zer0b2 <xrykin_00@mail.ru>
2022-12-29 19:29:09 +0300

    firts vue.js project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <MainComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainComponent from "./components/MainComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainComponent,
   },
 };
 </script>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.value">
+        {{ item.value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainComponent",
+  data() {
+    return {
+      text: "Firts Project",
+      items: [{ value: "Text 1" }, { value: "Text 2" }],
+    };
+  },
+};
+</script>
