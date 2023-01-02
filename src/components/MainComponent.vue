a7eadd6a599556cd7c4030cabe7208b4b06233d7
zer0b2 <xrykin_00@mail.ru>
2023-01-02 14:40:16 +0300

    add tasks.json in publc folder

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,44 +2,35 @@
   <div class="main">
     <ul id="item-list">
       <li
-        v-for="(item, index) in items"
-        :key="item.value"
+        v-for="(task, index) in tasks"
+        :key="task.task"
         :index="index"
         v-on:click="change_dec(index)"
         v-bind:class="[{ crossedout: item.isActive == true }]"
       >
-        {{ item.value }}
+        {{ task }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import jData from "../../public/doc_2022-12-30_13-32-06.eslintrc";
-
 export default {
   name: "mainComponent",
   data() {
-    return {
-      items: [
-        { value: "Text 1", isActive: false },
-        { value: "Text 2", isActive: false },
-      ],
-    };
+    return {};
   },
   methods: {
     change_dec: function (index) {
       console.log(this.items[index].isActive);
       this.items[index].isActive = !this.items[index].isActive;
     },
-    showData: function () {
-      console.log(jData.extends);
-      console.log(jData.parserOptions);
-      console.log(jData.rules);
+    getTasks() {
+      fetch("tasks.json");
     },
   },
-  beforeMount() {
-    this.showData();
+  mounted() {
+    this.getTasks();
   },
 };
 </script>
