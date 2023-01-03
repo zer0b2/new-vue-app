7632f2742da099f7e3c3735d7e39b61a5d80101a
zer0b2 <xrykin_00@mail.ru>
2023-01-03 20:47:40 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: './docs',
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://zer0b2.github.io/new-vue-app/docs/' : '/',
-  indexPath: '/new-vue-app/docs/index.html'
+  publicPath: process.env.NODE_ENV === 'production' ? '/new-vue-app/docs/' : '/',
 });
