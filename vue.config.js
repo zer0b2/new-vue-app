84797d73f69f8fab3e457c1836ad707a59526f47
zer0b2 <xrykin_00@mail.ru>
2023-01-03 22:27:17 +0300

    dep

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: './docs',
-  publicPath: ''
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/docs/'
+    : '/'
 });
