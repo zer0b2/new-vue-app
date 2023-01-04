22b5bb5f6bdbece8bcf6291eee20a64a267bf92e
zer0b2 <xrykin_00@mail.ru>
2023-01-04 16:00:41 +0300

    add currDay() in CalendarComponent

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,11 +1,25 @@
 <template>
   <h1>Календарь</h1>
+  <p>Сегодня {{ currDay() }}</p>
 </template>
 
 <script>
 export default {
   name: "calendatComponent",
   data() {},
+  methods: {
+    currDay() {
+      const current = new Date();
+      const date =
+        current.getDay() +
+        1 +
+        "." +
+        (current.getMonth() + 1) +
+        "." +
+        current.getFullYear();
+      return date;
+    },
+  },
 };
 </script>
 <style></style>
